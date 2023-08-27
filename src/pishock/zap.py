07e4734c5c2140c91da590bac46aac8a8010f771
Zerario <derg@zerario.dev>
2023-08-27 21:58:41 +0200

    Add old PiShock class

---
+++ src/pishock/zap.py
@@ -0,0 +1,54 @@
+import requests
+
+class Shocker:
+
+    NAME = "random"
+    OP_SHOCK = 0
+    OP_VIBRATE = 1
+    OP_BEEP = 2
+
+    def __init__(self, username: str, apikey: str, sharecode: str) -> None:
+        self.username = username
+        self.apikey = apikey
+        self.sharecode = sharecode
+
+    def shock(self, duration: int, intensity: int) -> bool:
+        return self._call(self.OP_SHOCK, duration=duration, intensity=intensity)
+
+    def vibrate(self, duration: int, intensity: int) -> bool:
+        return self._call(self.OP_VIBRATE, duration=duration, intensity=intensity)
+
+    def beep(self, duration: int) -> bool:
+        return self._call(self.OP_BEEP, duration=duration, intensity=None)
+
+    def _call(self, operation: int, duration: int, intensity: int | None) -> bool:
+        if not 0 <= duration <= 15:
+            raise ValueError(f"duration needs to be between 0 and 15, not {duration}")
+        if intensity is not None and not 0 <= intensity <= 100:
+            raise ValueError(
+                f"intensity needs to be between 0 and 100, not {intensity}"
+            )
+
+        assert (intensity is None) == (operation == self.OP_BEEP)
+        assert operation in [self.OP_BEEP, self.OP_SHOCK, self.OP_VIBRATE]
+
+        params = {
+            "Username": self.username,
+            "Name": self.NAME,
+            "Code": self.sharecode,
+            "Apikey": self.apikey,
+            "Duration": duration,
+            "Op": operation,
+        }
+        if intensity is not None:
+            params["Intensity"] = intensity
+
+        response = requests.post(
+            "https://do.pishock.com/api/apioperate", json=params
+        )
+
+        if response.status_code == 200 and response.text == "Operation Succeeded.":
+            return True
+
+        print(f"{response.status_code}: {response.text}")
+        return False
